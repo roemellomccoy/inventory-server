0c12ca8d5d150060f0a1692c41209829dceefbcf
roemellomccoy <roemello.mccoy22@gmail.com>
2021-07-21 13:54:22 -0500

    fixed seed

--- seeds/initial_helmets.js
+++ seeds/initial_helmets.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('helmets').del()
     .then(function () {
       // Inserts seed entries
       return knex('helmets').insert([
