52b5c794341f2d90c5b876bdd0511f6cf04fcf82
roemellomccoy <roemello.mccoy22@gmail.com>
2021-07-22 11:55:26 -0500

    added more gets

--- server.js
+++ server.js
@@ -7,13 +7,43 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors());
 
-app.get('/', function(req, res) {
+app.get('/helmets', function(req, res) {
     knex.select('*')
         .from('helmets')
         .then(data => res.status(200).json(data))
         .catch(err =>
             res.status(404).json({
                 message: 'No helmets'
+            }))
+})
+
+app.get('/chests', function(req, res) {
+    knex.select('*')
+        .from('chests')
+        .then(data => res.status(200).json(data))
+        .catch(err =>
+            res.status(404).json({
+                message: 'No chests'
+            }))
+})
+
+app.get('/arms', function(req, res) {
+    knex.select('*')
+        .from('arms')
+        .then(data => res.status(200).json(data))
+        .catch(err =>
+            res.status(404).json({
+                message: 'No arms'
+            }))
+})
+
+app.get('/legs', function(req, res) {
+    knex.select('*')
+        .from('legs')
+        .then(data => res.status(200).json(data))
+        .catch(err =>
+            res.status(404).json({
+                message: 'No legs'
             }))
 })
 
