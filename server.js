838479196357e2a8d7b0c6e0d135ae23aa08e1cb
roemellomccoy <roemello.mccoy22@gmail.com>
2021-07-25 16:04:20 -0500

    added patch

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.get('/', function(req, res) {
     })
 })
 
+app.patch('/', function(req, res){
+    res.send({
+        "name": req.body.name
+    })
+})
+
 app.get('/helmets', function(req, res) {
     knex.select('*')
         .from('helmets')
